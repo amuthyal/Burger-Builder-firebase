5fd4ac4da04c347548beabf8cca3e673b9af4e5f
akhila <amuthyal@asu.edu>
2019-07-12 22:48:39 -0700

    Add myClasses to the className

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classes from './BurgerIngredient.css'
+import myClasses from './BurgerIngredient.module.css'
 
 const burgerIngredient = (props) => {
     let ingredient = null;
     switch(props.type){
         case('bread-bottom'):
-            ingredient= <div className="BreadBottom"> </div>;
+            ingredient= <div className={myClasses.BreadBottom}> </div>;
             break;
         case('bread-top'):
             ingredient = (
-            <div className="BreadTop">
-                <div className="Seeds1"> </div>
-                <div className="Seeds2"> </div>
+            <div className={myClasses.BreadTop}>
+                <div className={myClasses.Seeds1}> </div>
+                <div className={myClasses.Seeds2}> </div>
             </div>
 
             );
             break;
         case('meat'):
-                ingredient = <div className= "Meat"> </div>;
+                ingredient = <div className= {myClasses.Meat}> </div>;
                 break;
         case('cheese'):
-                ingredient = <div className= "Cheese"> </div>;
+                ingredient = <div className= {myClasses.Cheese}> </div>;
                 break;
         case('salad'):
-                ingredient = <div className= "Salad"> </div>;
+                ingredient = <div className= {myClasses.Salad}> </div>;
                 break;
         case('bacon'):
-                ingredient = <div className= "Bacon"> </div>;
+                ingredient = <div className= {myClasses.Bacon}> </div>;
                 break;
         default: 
                 ingredient = null;
