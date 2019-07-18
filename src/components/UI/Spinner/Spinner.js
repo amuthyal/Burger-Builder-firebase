f64f819f8baaf13466c4db45d28a479b4577272c
akhila <amuthyal@asu.edu>
2019-07-17 23:57:48 -0700

    with HTTP post requests

---
+++ src/components/UI/Spinner/Spinner.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import myClasses from './Spinner.module.css';
+const spinner = () => (
+    <div className={myClasses.Loader}>Loading...</div>
+);
+
+export default spinner;
