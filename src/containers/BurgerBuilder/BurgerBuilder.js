3957006e4540e35a56943e64a2e0c6c0dc326399
akhila <amuthyal@asu.edu>
2019-07-19 12:54:46 -0700

    updated error

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -28,11 +28,12 @@ class BurgerBuilder extends Component{
     componentDidMount (){
         axios.get('https://react-my-burger-a644e.firebaseio.com/ingredients.json')
         .then(response => {
-            this.setState({ingredients: response.data})
+            this.setState({ingredients: response.data});
+        })
             .catch(error => {
                 this.setState({error: true});
             });
-        });
+       
 
     }
 
